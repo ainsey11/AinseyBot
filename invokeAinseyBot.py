673387be6a99947a25c6eb5b83324c0b2731eeaf
Robert Ainsworth <robert@ainsey11.com>
2017-03-17 20:10:24 +0000

    Removing port

--- invokeAinseyBot.py
+++ invokeAinseyBot.py
@@ -7,21 +7,21 @@ config = SafeConfigParser()
 config.read ('config/config.ini')
 
 Server = config.get("BotConnectionParams","Server")
-Port = config.get("BotConnectionParams","Port")
 Channel = config.get("BotConnectionParams","Channel")
 Nick = config.get("BotConnectionParams","Nick")
 AdminName = config.get("BotConnectionParams","AdminName")
 ExitMessage = config.get("BotConnectionParams","ExitMessage")
-Debug = config.get("BotDebugParams","EnableDebug")
+Debug = True #config.get("BotDebugParams","EnableDebug")
 DebugChannel = config.get("BotDebugParams","DebugChannel")
 if Debug == True:
-	ConnectChan = DebugChannel
-elseif Debug == False:
-	ConnectChan = Channel
+    ConnectChan = DebugChannel
+elif Debug == False:
+   ConnectChan = Channel
 
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-irc.connect((server,port))
-irc.send("USER "+Nick+" "+Nick+" "+Nick+" :AinseyBot\n")
-irc.send("NICK "+Nick+"\n")
-irc.send("JOIN "+ConnectChan+"\n")
+irc.connect((Server,6667))
+irc.send("USER" + Nick + Nick + Nick + ":AinseyBot\n")
+irc.send("NICK" + Nick +"\n")
+irc.send("JOIN" + ConnectChan +"\n")
+irc.send("PRIVMSG" + ConnectChan + ":Hello! I am a bot! \r\n")
 
