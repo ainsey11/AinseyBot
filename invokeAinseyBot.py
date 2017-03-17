3bf510e3fba819affc4bddad56e2a890e4827d01
Robert Ainsworth <robert@ainsey11.com>
2017-03-17 16:33:41 +0000

    Added Channel into docs, and added into get vars, started basic connection into irc via socket

--- invokeAinseyBot.py
+++ invokeAinseyBot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import socket
+import sys
 from ConfigParser import SafeConfigParser
 
 config = SafeConfigParser()
@@ -7,6 +8,14 @@ config.read ('config/config.ini')
 
 Server = config.get("BotConnectionParams","Server")
 Port = config.get("BotConnectionParams","Port")
+Channel = config.get("BotConnectionParams","Channel")
 Nick = config.get("BotConnectionParams","Nick")
 AdminName = config.get("BotConnectionParams","AdminName")
 ExitMessage = config.get("BotConnectionParams","ExitMessage")
+
+irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+irc.connect((server,port))
+irc.send("USER "+Nick+" "+Nick+" "+Nick+" :AinseyBot\n")
+irc.send("NICK "+Nick+"\n")
+irc.send("JOIN "+Channel+"\n")
+
