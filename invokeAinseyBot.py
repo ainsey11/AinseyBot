4714522c63599500b2b3a00dc858e117844cb226
Robert Ainsworth <robert@ainsey11.com>
2017-03-17 15:59:01 +0000

    Basic paramaters now loads from config file

--- invokeAinseyBot.py
+++ invokeAinseyBot.py
@@ -7,4 +7,6 @@ config.read ('config/config.ini')
 
 Server = config.get("BotConnectionParams","Server")
 Port = config.get("BotConnectionParams","Port")
-
+Nick = config.get("BotConnectionParams","Nick")
+AdminName = config.get("BotConnectionParams","AdminName")
+ExitMessage = config.get("BotConnectionParams","ExitMessage")
