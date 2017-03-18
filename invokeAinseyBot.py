9916e6cc4f5586a07503eb36354478bb4e79725f
Robert Ainsworth <robert@ainsey11.com>
2017-03-18 01:19:18 +0000

    This shit is broken, going to bed, dont care anymore

--- invokeAinseyBot.py
+++ invokeAinseyBot.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 import socket
 import sys
 import time
+import urllib2
+import re
 from ConfigParser import SafeConfigParser
+from BeautifulSoup import BeautifulSoup
 
 config = SafeConfigParser()
 config.read ('config/config.ini')
@@ -13,9 +17,11 @@ Nick = config.get("BotConnectionParams","Nick")
 IdentifyPassword = config.get("BotConnectionParams","IdentifyPassword")
 AdminName = config.get("BotConnectionParams","AdminName")
 ExitMessage = config.get("BotConnectionParams","ExitMessage")
-Debug = False 
+Debug = bool(config.get("BotDebugParams","EnableDebug"))
 DebugChannel = config.get("BotDebugParams","DebugChannel")
 WaveActivator = config.get("BotActivators","WaveActivator")
+EnableURLPlugin = bool(config.get("BotPlugins","EnableURLPlugin"))
+
 print ("Starting " + Nick + " up! I'm firin mah lazah!")
 
 if Debug == True:
@@ -23,6 +29,11 @@ if Debug == True:
 	print "Debug is Enabled, console will be spammed with junk shortly, good luck captain"
 	print ("Connecting to : " + ConnectChan + " for my channel")
 	print ("DEBUG: Using " + Nick + " and the identifer password " + IdentifyPassword + " to connect to " + Server)
+	if EnableURLPlugin == True:	
+		print ("DEBUG: EnableURLPlugin is Enabled")
+	elif  EnableURLPlugin == False:
+		print ("DEBUG: EnableURLPlugin is Disabled")
+
 elif Debug == False:
 	ConnectChan = Channel
 	print "I see you don't wanna debug, that means I'm stable, woooooo! *pops pills*"
@@ -40,10 +51,11 @@ irc.send('PRIVMSG ' + ConnectChan + ' :All rise, the bot has joined the channel\
 
 while True:
 	data = irc.recv (4096)
-	print data
-	
+	if data.find('PING') != -1:
+		irc.send('PONG ' + data.split()[1] + '\r\n') 
 	if data.find(':'+WaveActivator) !=-1:
-		t = data.split(':'+WaveActivator)
-		to = t[1].strip()
 		irc.send('PRIVMSG ' + ConnectChan + ' :' + Nick + ' waves hello' '\r\n')
-	
+	if EnableURLPlugin == True:
+		if data.find(':http') !=-1:
+			
+			
